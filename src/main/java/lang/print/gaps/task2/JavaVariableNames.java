e9d4a125704d35e152e3a4296b7783c0e519c86a
Valerii Krutoshynskyi <valera9496@gmail.com>
2022-12-01 15:37:41 +0100

    Task 3.2/1

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -6,7 +6,7 @@ public class JavaVariableNames {
         int $money = 10;
         int hundred = 100;
         int back = 1000;
-        int underscore = 10000;
+        int _underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
         int BAD_EXAMPLE_OF_VAR_NAME = 1000000;
