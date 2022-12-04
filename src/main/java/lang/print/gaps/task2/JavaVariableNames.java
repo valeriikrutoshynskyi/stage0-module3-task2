554fb55e57e34a6edf7666a3ce4381402e573c98
Valerii Krutoshynskyi <valera9496@gmail.com>
2022-12-04 15:06:49 +0100

    Done?

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int morrow = 0;
-        int money = 10;
+        int $money = 10;
         int hundred = 100;
         int back = 1000;
         int underscore = 10000;
