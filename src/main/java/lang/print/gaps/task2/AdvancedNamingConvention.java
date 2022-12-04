1c77bc0b887d59292b854c3a5da67d12a58301ba
Valerii Krutoshynskyi <valera9496@gmail.com>
2022-12-04 16:57:26 +0100

    1

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
-    private static final int ADULT_AGE= 18;
+    private static final int ADULT_AGE = 18;
 
     private int age;
     void callToFriend() {
